1919964b0dabe984405fda4684b767361a304288
John Leavell <johnleavell@gmail.com>
2022-07-19 21:52:07 -0500

    removed old comments

--- app/src/components/pages/Sample/index.js
+++ app/src/components/pages/Sample/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import sampleAPI from "../../../utils/sampleAPI";
+// import sampleAPI from "../../../utils/sampleAPI";
 import { useForm } from "react-hook-form";
 
 const Sample = () => {
@@ -71,26 +71,3 @@ const Sample = () => {
 };
 
 export default Sample;
-// dateCollected: req.body.dateCollected,
-// timeCollected: req.body.timeCollected,
-// sampleVolume: req.body.sampleVolume,
-// samplePh: req.body.samplePh,
-// user: req.body.user,
-// manholeIdNumber: req.body.manholeIdNumber,
-// sampleTemperature: req.body.sampleTemperature,
-// Location Name [as a drop down]
-
-// timezone seletion
-
-//  sampling methond 4 radio buttons
-
-// sample date and timezone
-// sample collection and processing information
-
-//temp yes /no
-
-//transpprt yes/no
-
-// name of person filling out form
-
-// notes
